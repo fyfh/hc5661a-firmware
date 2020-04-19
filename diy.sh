5b3e492e1ab64cc70562b587331066731cf62c4a
fyfh <13757260730@sina.cn>
2020-04-19 18:31:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,7 +8,3 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-mkdir package/base-files/files/config
-echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
-echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
