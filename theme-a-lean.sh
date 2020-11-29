885269da6f127f204c8b905f0a2a0f8dc767b29f
fyfh <13757260730@sina.cn>
2020-11-29 18:30:26 +0800

    Update theme-a-lean.sh

--- theme-a-lean.sh
+++ theme-a-lean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd lede/package/lean  
+cd openwrt/package/lean  
 rm -rf luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 ./scripts/feeds update -a
