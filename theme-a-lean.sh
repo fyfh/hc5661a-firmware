db7f9ac1512de1b6d48b58bf63f1747f20201993
fyfh <13757260730@sina.cn>
2020-11-29 18:22:34 +0800

    Update theme-a-lean.sh

--- theme-a-lean.sh
+++ theme-a-lean.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd lede/package/lean  
 rm -rf luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+./scripts/feeds update -a
+./scripts/feeds install -a
